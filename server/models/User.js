b41b54752a4286e29e6599b0977d78101fd115d4
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-11 19:08:07 +0530

    schema added to the page

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true ,
+        
+    },
+    email: {
+        type: String,
+        required: true ,
+        unique: true,
+    },
+
+
+    password: {
+        type: String,
+        required: true ,
+    },
+    dob: {
+        type: Date,
+        required: true , 
+    },
+
+});
+
+const User = model("user", UserSchema);
+
+
+export default User;
+
+
