7138bfd8ce75a1d290f5eaae02ff6b90f3230b37
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-14 13:33:11 +0530

    frontend added

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {RouterProvider,createBrowserRouter} from 'react-router-dom';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import "./global.css"
 
+import Home from "./views/Home/Home"
+import Login from "./views/Login/Login"
+import Signup from "./views/Signup/Signup"
+import AddTransaction from "./views/AddTransaction/AddTransaction"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createBrowserRouter([
   {
-  path: '/',
-  element: <h1>Home</h1>
-},
-{
-  path: '*',
-  element: <h1>Not found</h1>
-},
-
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: '/add-transaction',
+    element: <AddTransaction />
+  },
+  {
+    path: '*',
+    element: <h1>Not Found</h1>
+  }
 ])
 
-
-root.render(<RouterProvider router={router}/>);
+root.render(<RouterProvider router={router} />);
