94932e36e4e56f501ca7731e1545015d551086fd
KHUSHI 444 <khankhushi4452@gmail.com>
2024-07-19 09:25:25 +0530

    changes in index page

--- server/index.js
+++ server/index.js
@@ -1,43 +1,17 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-import { postSignup, postLogin } from './controllers/user.js';
-
-import { postTransaction, getTransactions } from "./controllers/transaction.js";
-
-
-// Connect to MongoDB
-const connectDB = async () =>{
-  const conn = await mongoose.connect(process.env.MONGODB_URI)
-
-  if (conn) {
-    console.log(`MongoDB connected successfully😊😊`);
-  }
-};
-connectDB();
-
-
-app.get('/', (req, res) => {
-  res.json({
-    message: `Welcome to Expense Tracker API`
-  })
-})
-
-app.post("/signup", postSignup)
-app.post("/login", postLogin)
-
-app.post("/transaction", postTransaction)
-app.get("/transactions", getTransactions)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, ()=>{
+  console.log(`server is runnning on port ${PORT}`);
 })
